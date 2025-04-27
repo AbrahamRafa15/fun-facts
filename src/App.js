2e9cb82a61ec91bb252fc98fea34780748a31f53
AbrahamMartinez15 <abraham.martinez1503@gmail.com>
2025-04-27 17:57:24 -0600

    Build the web page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { animals } from "./animals";
+import React, { useState } from "react";
 
-function App() {
+
+const App = () => {
+  const [fact, setFact] = useState("");
+  const showBackground = true;
+  const title = "";
+
+  const displayFact = (animal) => {
+    const facts = animals[animal].facts;
+    const randomFact = facts[Math.floor(Math.random() * facts.length)];
+    console.log(randomFact);
+    setFact(randomFact);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{title || "Click an animal for a fun fact"}</h1>
+      {showBackground && (
+        <img className="background" alt="ocean" src="/images/ocean.jpg" />
+      )}
+      <p id="fact">{fact}</p>
+      <div className="animals">
+        {Object.entries(animals).map(([name, { image }]) => (
+          <img
+            key={name}
+            className="animal"
+            alt={name}
+            src={image}
+            aria-label={name}
+            role="button"
+            onClick={() => displayFact(name)}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
