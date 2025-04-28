cf6ee45ebcd2b679a2527bfb1bbd784d7f5c0d68
AbrahamMartinez15 <abraham.martinez1503@gmail.com>
2025-04-28 10:52:26 -0600

    Update App.js to use dynamic background

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import React, { useState } from "react";
 
 const App = () => {
   const [fact, setFact] = useState("");
-  const showBackground = true;
-  const title = "";
+  const [showBackground, setShowBackGround] = useState(true);
+  const title = "Click an animal for a fun fact";
 
   const displayFact = (animal) => {
     const facts = animals[animal].facts;
@@ -16,7 +16,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>{title || "Click an animal for a fun fact"}</h1>
+      <h1>{title}</h1>
       {showBackground && (
         <img className="background" alt="ocean" src="/images/ocean.jpg" />
       )}
@@ -34,6 +34,9 @@ const App = () => {
           />
         ))}
       </div>
+      <button onClick={() => setShowBackGround(!showBackground)}>
+          Toogle background
+      </button>
     </div>
   );
 };
